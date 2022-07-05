641c13bb39318d1770f9f84ffa4004f16f40e4c2
maximozanetta <maximozanetta@gmail.com>
2022-07-05 12:50:09 -0300

    NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand text-white" href="#">Tienda</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav mx-5">
+                        <a className="nav-link text-white" aria-current="page" href="#">Home</a>
+                        <a className="nav-link text-white" href="#">Products</a>
+                        <a className="nav-link text-white" href="#">Contact</a>
+                        <a className="nav-link text-white"href="#">About us</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
