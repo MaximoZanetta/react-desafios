adf7f0293d704a37cf7a5c0e02a5bc79178ebf14
maximozanetta <maximozanetta@gmail.com>
2022-08-08 15:54:22 -0300

    item collection

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,32 +3,28 @@ import ItemList from "./ItemList";
 import data from "../data"
 import { useParams } from "react-router-dom";
 import Spinner from "./Spinner";
+import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
 
 const ItemListContainer = () => {
   
-      const { name } = useParams()
-      const [items, setItems]= useState([])
-      const [loading, setLoading] = useState(false)
-
-      const promise = new Promise((resolve)=>{
-        setTimeout(
-          ()=>{
-              resolve(data)
-          },2000)
-      })
+  const { name } = useParams()
+  const [items, setItems]= useState([])
+  const [loading, setLoading] = useState(false)      
       
-
-      useEffect(()=>{
-      setLoading(true)
-      promise.then((res)=> {
-        const products = res;
-        if(name) {
-              setItems(products.filter((product) => product.car == name))
-            } else {
-              setItems(products)
-            }
-            setLoading(false)
-          })
+  useEffect(()=> {
+    setLoading(true)
+    const querydb = getFirestore()
+    const queryCollection = collection(querydb, 'products');
+    if(name) {
+      const queryFilter = query(queryCollection, where('model','==', name))
+      getDocs(queryFilter)
+      .then(res => setItems(res.docs.map(product => ({id: product.id, ...product.data()}))))
+      } else {
+        getDocs(queryCollection) 
+        .then(res => setItems(res.docs.map(product => ({id: product.id, ...product.data()}))))
+        // setItems(products)
+      }
+        setLoading(false)
       },[name])
 
       if(loading){
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxIqORjTCAxhRl4WqrBtg9l4lQ3i0x7io",
+  authDomain: "ecommercemz.firebaseapp.com",
+  projectId: "ecommercemz",
+  storageBucket: "ecommercemz.appspot.com",
+  messagingSenderId: "334924524264",
+  appId: "1:334924524264:web:cff089f31dffa5db0cb75d"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
